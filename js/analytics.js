d015a2360234b42f4e43c20bff8e1e34edad648b
jeremiah9980 <49728959+jeremiah9980@users.noreply.github.com>
2026-03-10 13:48:03 -0500

    Add Google Analytics tracking script

---
+++ js/analytics.js
@@ -0,0 +1,14 @@
+const GA_ID = "G-XXXXXXXX";
+
+var script = document.createElement("script");
+script.async = true;
+script.src = "https://www.googletagmanager.com/gtag/js?id=" + GA_ID;
+
+document.head.appendChild(script);
+
+window.dataLayer = window.dataLayer || [];
+
+function gtag(){dataLayer.push(arguments);}
+
+gtag('js', new Date());
+gtag('config', GA_ID);
